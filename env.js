26ae3ed60d6285d730dd63b134884e7edf36038f
Wésley Queiroz <wesley.coder@gmail.com>
2018-03-02 15:47:39 -0300

    now.json

--- env.js
+++ env.js
@@ -4,8 +4,8 @@ export const env = {
 
 env.domain = `${env.prod ? 'poke-shop.now.sh': 'localhost'}`
 env.appPort = process.env.PORT || (env.prod ? 80 : 3000)
-env.appUrl = `https://${env.domain}:${env.appPort}`
-env.paymentEndpoint = process.env.PAYMENT_API || 'https://api.pagar.me/1/transactions'
+env.appUrl = `https://${env.domain}${env.appPort !== 80 ? `:${env.appPort}` : ''}`
+env.paymentEndpoint = process.env.PAYMENT_API
 env.paymentAPIKey = process.env.PAYMENT_API_KEY
 
 export default env
